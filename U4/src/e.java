505d18fdf36f36ac36885653d33df5435c3e506f
Ostabo <osi.borkenhagen@gmail.com>
2022-11-24 16:13:20 +0100

    add correct 4e

--- U4/src/e.java
+++ U4/src/e.java
@@ -2,6 +2,7 @@ import org.hibernate.Query;
 import org.orm.PersistentSession;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class e {
     public static void main(String[] args) {
@@ -18,6 +19,20 @@ public class e {
                                 .map(Vorlesung::getEcts)
                                 .mapToInt(Integer::intValue)
                                 .sum());
+            });
+
+            System.out.println();
+
+            Query q = session.createQuery(
+                    "select s.name, sum(v.ects) " +
+                            "from Studiengang s, Vorlesung v " +
+                            "where v.studiengang = s " +
+                            "group by s.name"
+            );
+
+            List<Object[]> x = q.list();
+            x.forEach(res -> {
+                System.out.printf("%26s%10s ECTS-Summe\n", res[0], res[1]);
             });
 
         } catch (Exception e) {
